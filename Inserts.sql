4cb6699678d3a982c3c35251e883708349cf0c46
Anast <nastya_strong@ukr.net>
2019-04-09 20:41:21 +0300

    add left-side inserts

--- Inserts.sql
+++ Inserts.sql
@@ -1,10 +1,25 @@
 --RIGHT SIDE
 INSERT INTO guide_position(position_name)
-VALUES("chief"),
-("helper"),
-("intern");
-INSERT INTO guide()
-SELECT * FROM guide_position;
+VALUES("chief"),("helper"),("intern");
+
+INSERT INTO guide(id_position, firstname, lastname)
+values(1,"Taras","Tarasenko"),(2,"Petro","Petrenko"),(2,"Ivan","Ivanenko"),
+(3,"Pavlo","Pavlenko"),(3,"Ostap","Ostapenko");
+
+INSERT INTO excursions(excursion_name)
+VALUES("Ancient Greece"),("Ancient Rome"),("Ancient Egypt"),("Mesopotamia"),("Middle Ages");
+
+
+INSERT INTO schedules(id_excursion, id_guide, time_start, time_end)
+VALUES(1,2,'2019-08-01 09:00:00','2019-08-01 12:00:00'),
+(2,1,'2019-08-01 11:00:00','2019-08-01 12:00:00'),
+(3,4,'2019-08-01 10:00:00','2019-08-01 11:00:00'),
+(4,3,'2019-08-01 11:00:00','2019-08-01 12:00:00'),
+(1,4,'2019-08-01 14:00:00','2019-08-01 15:00:00'),
+(2,3,'2019-08-01 17:00:00','2019-08-01 18:30:00'),
+(3,2,'2019-08-01 13:00:00','2019-08-01 13:30:00'),
+(4,1,'2019-08-01 14:00:00','2019-08-01 14:30:00');
+
 
 
 
