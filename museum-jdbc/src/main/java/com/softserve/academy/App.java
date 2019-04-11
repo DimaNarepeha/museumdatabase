304c62285dd2c1e4c1388540d097863056b9d952
Quadhands <dimanarepeha@gmail.com>
2019-04-11 16:24:29 +0300

    Add basic menu without implementation

--- museum-jdbc/src/main/java/com/softserve/academy/App.java
+++ museum-jdbc/src/main/java/com/softserve/academy/App.java
@@ -1,6 +1,7 @@
 package com.softserve.academy;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -21,10 +22,88 @@ public class App {
 
     }
 
-    public static void printMenu() {
+    public static void menu(BufferedReader bufferedReader) {
         System.out.println("1.CREATE(write data)\n"
                 + "2.READ\n"
                 + "3.UPDATE\n"
                 + "4.DELETE");
+        try {
+            int input = Integer.parseInt(bufferedReader.readLine());
+            switch(input){
+                case 1:
+                    createMenu(bufferedReader);
+                    break;
+                case 2:
+                    readMenu(bufferedReader);
+                    break;
+                case 3:
+                    updateMenu(bufferedReader);
+                    break;
+                case 4:
+                    deleteMenu(bufferedReader);
+                    break;
+                    default:
+                        System.out.println("Invalid number. Please Enter again");
+                        menu(bufferedReader);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void createMenu(BufferedReader bufferedReader) {
+        System.out.println("1.CREATE(write data)\n"
+                + "2.READ\n"
+                + "3.UPDATE\n"
+                + "4.DELETE");
+        try {
+            int input = Integer.parseInt(bufferedReader.readLine());
+            switch(input){
+                case 1:
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void readMenu(BufferedReader bufferedReader) {
+        System.out.println("1.CREATE(write data)\n"
+                + "2.READ\n"
+                + "3.UPDATE\n"
+                + "4.DELETE");
+        try {
+            int input = Integer.parseInt(bufferedReader.readLine());
+            switch(input){
+                case 1:
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void updateMenu(BufferedReader bufferedReader) {
+        System.out.println("1.CREATE(write data)\n"
+                + "2.READ\n"
+                + "3.UPDATE\n"
+                + "4.DELETE");
+        try {
+            int input = Integer.parseInt(bufferedReader.readLine());
+            switch(input){
+                case 1:
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void deleteMenu(BufferedReader bufferedReader) {
+        System.out.println("1.CREATE(write data)\n"
+                + "2.READ\n"
+                + "3.UPDATE\n"
+                + "4.DELETE");
+        try {
+            int input = Integer.parseInt(bufferedReader.readLine());
+            switch(input){
+                case 1:
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
